8f5a1da9d18ddb5c8c5bec8b26aacd948faf371c
Ilya Chudinovskikh <87187545+Ilya-Chudinovskikh@users.noreply.github.com>
2022-01-27 18:14:57 +0300

    The SharedModelsLibrary were added for interaction between projects and RabbitMQ.

--- WordOfTheDay.AnaliticsMicroservice.Repository/WordConsumer.cs
+++ WordOfTheDay.AnaliticsMicroservice.Repository/WordConsumer.cs
@@ -1,13 +1,13 @@
-﻿using WordOfTheDay.AnaliticsMicroservice.Repository.Entities;
+﻿using SharedModelsLibrary;
 using MassTransit;
 using System;
 using System.Threading.Tasks;
 
 namespace WordOfTheDay.AnaliticsMicroservice.Repository
 {
-    internal class WordConsumer : IConsumer<Word>
+    internal class WordConsumer : IConsumer<WordInfo>
     {
-        public async Task Consume(ConsumeContext<Word> context)
+        public async Task Consume(ConsumeContext<WordInfo> context)
         {
             await Console.Out.WriteLineAsync(context.Message.Text);
         }
